819a504ef163f215ffc574b999e9dacc072ab8a1
Freifunk 3Laendereck <admin@freifunk-3laendereck.de>
2015-06-27 00:30:16 +0200

    use neutral addresses & introduce syntax highlighting

---
+++ test/init.pp
@@ -0,0 +1,41 @@
+node default {
+  radvd::interface { 'eth0':
+    options  => {
+      'AdvSendAdvert' => 'on',
+    },
+    prefixes => {
+      '2001:bf7:20::/64' => {},
+    },
+  }
+
+  radvd::interface { 'eth1':
+    options  => {
+      'AdvSendAdvert'     => 'on',
+      'MinRtrAdvInterval' => 10,
+      'MaxRtrAdvInterval' => 30,
+    },
+    prefixes => {
+      '2001:bf7:20::/64' => {
+        'AdvOnLink'     => 'on',
+        'AdvAutonomous' => 'on',
+      },
+    },
+    rdnss    => {
+      '2001:bf7:20::1' => {
+        'AdvRDNSSLifetime' => 30,
+      },
+    },
+    dnssl    => {
+      'branch.example.com example.com' => {
+        'AdvDNSSLLifetime' => 30,
+      },
+    },
+    routes   => {
+      '2001:0DB8:2342:ccc::/64' => {},
+    },
+    clients  => [
+      'fe80::21f:16ff:fe06:3aab',
+      'fe80::21d:72ff:fe96:aaff',
+    ],
+  }
+}
